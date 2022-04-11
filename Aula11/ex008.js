51e594deb4f5cb0fac81921ca81ffa6349ab4aa2
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-10 23:21:02 -0300

    Exercicio 008 Js

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+  // condição simples!
+  console.log('Voce ultrapassou a velocidade permitida, Multado!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
