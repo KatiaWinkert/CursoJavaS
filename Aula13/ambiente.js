0862f2562e5821b2567fed2f18fe12672bb33c40
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-12 22:55:14 -0300

    Estrutura de Repetição
    adicionada a Estrutura Do

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,8 +1,16 @@
 var c = 1
+do {
+  console.log(`Passo ${c}`)
+  c++ // siguinifica que c = c +1 (c recebe c mais um)
+} while (c <= 2)
+
+/*
+var c = 1
 while (c <= 500) {
   console.log(`Passo ${c}`)
   c++ // siguinifica que c = c +1 (c recebe c mais um)
 }
+*/
 
 /*
 console.log('tudo bem?')
