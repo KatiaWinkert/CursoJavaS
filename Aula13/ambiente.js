939e3816034a3df9a5ed4132afa164bbf38a244d
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-12 22:41:01 -0300

    Aula 13 Atividade
    Exercício sobre estrutura de repetição While

---
+++ Aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+while (c <= 500) {
+  console.log(`Passo ${c}`)
+  c++ // siguinifica que c = c +1 (c recebe c mais um)
+}
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
