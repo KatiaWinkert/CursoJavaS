95a2cf7ee61c594e5cdea57aca330b44bfbc1a12
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-13 21:05:29 -0300

    commit

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,2 +1,7 @@
-let num = [5,2,8,9,3]
-console.log(`Nosso vetor é o ${num}`)
+let num = [5, 2, 8, 9, 3]
+num.push(1) // acrescenta valor no array
+num.sort() // sort coloca o vetor em ordem
+console.log(`o Vetor tem  ${num.length} posições`) // leangth informa quantas posiçoes tem o array
+console.log(num[0])
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(num)
