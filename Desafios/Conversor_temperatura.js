04936d5f0d4fded2a295e8ea837bee0a4ffaf40d
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-17 19:49:18 -0300

    commit

--- Desafios/Conversor_temperatura.js
+++ Desafios/Conversor_temperatura.js
@@ -1,9 +1,11 @@
 let bt = document.querySelector('input#bt')
 let res = document.querySelector('div#res')
 let conv = document.querySelector('div#conversao')
-bt.addEventListener('click', clicar)
+bt.addEventListener('click', clicar) 
 
 function clicar(){
-let 
+    let temperature = Number(prompt("Digite uma temperatura em *C (Celsius)"))
+let convtofarenheit = 
 
+    
 }
