24e22cccbaf34bcbc538b105a6550a257f9e7326
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-17 00:13:55 -0300

    Homework 6
    atividade conversão de temperatura.

---
+++ Desafios/Conversor_temperatura.js
@@ -0,0 +1,9 @@
+let bt = document.querySelector('input#bt')
+let res = document.querySelector('div#res')
+let conv = document.querySelector('div#conversao')
+bt.addEventListener('click', clicar)
+
+function clicar(){
+let 
+
+}
