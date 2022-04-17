1991f058de73801b8713b1713bca6bb6f512590d
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-16 23:08:05 -0300

    Homework concluded

--- Desafios/conversor_medidas.js
+++ Desafios/conversor_medidas.js
@@ -1,21 +1,21 @@
+let bt = document.querySelector('input#bt')
 let res = document.querySelector('div#res')
-let conver = document.querySelector('div#conversao')
-
+let conv = document.querySelector('div#conversao')
 bt.addEventListener('click', clicar)
 
 function clicar() {
-  let M = Number(prompt('Digite A Distancia em metros(m)'))
+  let m = Number(prompt('Digite A Distancia em metros(m)'))
   let Km = (m / 1000).toFixed(3) // .tofixed() Ele determina a quantidade de casas decimais que preciso no programa.//
-  let Hm = (M / 100).toFixed(3)
-  let Dam = (M / 10).toFixed(3)
-  let Dm = M * 10
-  let Cm = M * 100
-  let mm = M * 1000
-  res.innerHTML = `A distancia de ${M} metros, corresponde a ...`
-  conver.innerHTML = `  <p>${Km} Quilometros (Km)<p>.
-                        <p>${Hm} Hectômetros (Hm).
-                        <p>${Dam} Decâmetros (Dam)
-                        <p>${Dm} Decímetro (Dm)
-                        <p>${Cm} Centimetros (CM)
-                        <p>${mm} Milimetros (mm)`
+  let Hm = (m / 100).toFixed(3)
+  let Dam = (m / 10).toFixed(3)
+  let Dm = m * 10
+  let Cm = m * 100
+  let mm = m * 1000
+  res.innerHTML = `<h2>A distancia de ${m} metros, corresponde a ... <h2>`
+  conv.innerHTML = `  <p>${Km} Quilometros (Km)<p>
+                        <p>${Hm} Hectômetros (Hm)<p>
+                        <p>${Dam} Decâmetros (Dam)<p>
+                        <p>${Dm} Decímetro (Dm)<p>
+                        <p>${Cm} Centimetros (CM)<p>
+                        <p>${mm} Milimetros (mm)<p>`
 }
