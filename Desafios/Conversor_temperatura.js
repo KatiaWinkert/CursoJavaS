8565c7ae11925f857302bbe67ea182e90eb4e5b7
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-17 20:49:15 -0300

    concluded Homework 6
    desafio 6 concluído. Conversão da temperatura

--- Desafios/Conversor_temperatura.js
+++ Desafios/Conversor_temperatura.js
@@ -1,11 +1,14 @@
 let bt = document.querySelector('input#bt')
 let res = document.querySelector('div#res')
 let conv = document.querySelector('div#conversao')
-bt.addEventListener('click', clicar) 
+bt.addEventListener('click', clicar)
 
-function clicar(){
-    let temperature = Number(prompt("Digite uma temperatura em *C (Celsius)"))
-let convtofarenheit = 
+function clicar() {
+  let temperature = Number(prompt('Digite uma temperatura em *C (Celsius)'))
+  let convtofahrenheit = temperature * 1.8 + 32
+  let kelvinconv = temperature + 273.15
 
-    
+  res.innerHTML = `<h2>A temperatura de ${temperature}, corresponde a ...<h2>`
+  conv.innerHTML = `<strong>${convtofahrenheit}'F (Fahrenheit) </strong> </br>
+                    <strong>${kelvinconv}'K (Kelvin)</strong>`
 }
