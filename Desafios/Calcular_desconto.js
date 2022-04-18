c4c11e78cef2cfcd195aee3c04e5fe5abc1976b1
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-18 00:24:14 -0300

    Homework Concluded 8
    Concluído desafio da calculadora de desconto - Curso em video

--- Desafios/Calcular_desconto.js
+++ Desafios/Calcular_desconto.js
@@ -6,12 +6,12 @@ bt.addEventListener('click', clicar)
 
 function clicar() {
   let produto = prompt('Qual é o produto que você esta comprando?')
-  let valor = prompt(`Qual é o preço do ${produto}`)
-  let desconto = (valor / 10).toFixed(2) //calculando desconto
-  let des = (valor - desconto).toFixed(2) // desconto dado
+  let valor = Number(prompt(`Qual é o preço do ${produto}`))
+  let desconto = (valor/10).toFixed(2) //calculando desconto
+  let des = (valor-desconto).toFixed(2) // desconto dado
 
   res.innerHTML = `<h2>Calculando desconto de 10% para ${produto}: <h2>`
-  conv.innerHTML = `<p> O preço original do produto era <strong>'${produto}' reais</strong>. <p>
+  conv.innerHTML = `<p> O preço original do produto era <strong>'${valor}' reais</strong>. <p>
                     <p> Voce acaba de ganhar <strong>${desconto}</strong> reais de desconto (-10%).<p>
                     <p> Você pagará <strong>${des}</strong> reais no produto <strong>'${produto}'</strong>.<p>`
 }
