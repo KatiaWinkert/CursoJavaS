3c98c1f314bf720eccaf5561f81ec9943d7a5cdb
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-17 22:51:32 -0300

    Homework concluded 7
    Conclusão do exercício conversão Monetária - desafio 7 curso em video

--- Desafios/conversor_monetario.js
+++ Desafios/conversor_monetario.js
@@ -1,3 +1,23 @@
-let cotação = Number(
+let cota = Number(
   prompt('Antes de mais nada. Quando está a cotação do dolar agora?')
 )
+let bt = document.querySelector('input#bt')
+let res = document.querySelector('div#res')
+let conv = document.querySelector('div#conversão')
+
+bt.addEventListener('click', clicar)
+
+function clicar() {
+  let reais = Number(prompt('Quantos reais tem na sua carteira?'))
+  let dolar = (reais / cota).toFixed(2)
+
+  if (reais > 4.7) {
+    res.innerHTML = `<h2>Se você tem ${reais} na carteira, você pode obter US$ ${dolar} dolares Americanos`
+  } else if (reais == 4.7) {
+    res.innerHTML = `<h2>Se você tem ${reais} na carteira, você pode obter US$ ${dolar} dolares Americanos`
+  } else if (reais < 4.7 && reais > 1) {
+    res.innerHTML = `<h2>Se você tem ${reais} na carteira, você pode obter US$ ${dolar} Cents Americanos`
+  } else if (reais == 1) {
+    res.innerHTML = `<h2>Se você tem ${reais} na carteira, você pode obter US$ ${dolar} cents Americanos`
+  }
+}
