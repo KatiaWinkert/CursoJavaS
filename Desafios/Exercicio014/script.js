4a1a8b1ce55115aca330ca3bcafe7201155fe145
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-19 17:08:06 -0300

    commit
    exercício em execução 2

--- Desafios/Exercicio014/script.js
+++ Desafios/Exercicio014/script.js
@@ -1,7 +1,21 @@
 function carregar() {
-  var msg = window.document.querySelector('div#msg')
-  var img = window.document.querySelector('div#imagem')
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('imagem')
   var data = new Date()
   var hora = data.getHours()
-  msg.innerHTML = `Agora são ${hora} horas.`
+ 
+  msg.innerHTML = `<strong> Agora são ${hora} horas</strong>.`
+  if (hora >= 0 && hora < 12) {
+    //vair se Bom dia !!!
+    img.src = 'fotomanha.png'
+    document.body.style.background = '#7ec9fa'
+  } else if (hora >= 12 && hora <= 18) {
+    //vai ser boa tarde!!!
+    img.src = 'fototarde.png'
+    document.body.style.background = '#fe5a02'
+  } else {
+    //Se não será Boa noite!!!
+    img.src = 'fotonoite.png'
+    document.body.style.background = '#000f13'
+  }
 }
