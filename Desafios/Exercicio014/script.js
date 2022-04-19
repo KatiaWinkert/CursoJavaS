66372158033f8a38d7fc46aa8f7a76877cccdf5a
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-19 00:15:13 -0300

    commit

--- Desafios/Exercicio014/script.js
+++ Desafios/Exercicio014/script.js
@@ -1,5 +1,7 @@
-function carregar(){
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-
+function carregar() {
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('imagem')
+  var date = new Date()
+  var hora = data.gethours()
+  msg.innerHTML = `Agora são ${hora} horas.`
 }
