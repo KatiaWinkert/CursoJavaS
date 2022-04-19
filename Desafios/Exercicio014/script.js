f015f8dba44534a5df363c3e03987933954932cb
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-19 15:16:58 -0300

    commit
    exercício em execução

--- Desafios/Exercicio014/script.js
+++ Desafios/Exercicio014/script.js
@@ -1,7 +1,7 @@
 function carregar() {
-  var msg = window.document.getElementById('msg')
-  var img = window.document.getElementById('imagem')
-  var date = new Date()
-  var hora = data.gethours()
+  var msg = window.document.querySelector('div#msg')
+  var img = window.document.querySelector('div#imagem')
+  var data = new Date()
+  var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
 }
