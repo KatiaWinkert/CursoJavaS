574f08bdbf7de5094ec0698bba7c4d79b868e31a
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-20 00:47:46 -0300

    Verificador de Idade concluded
    Atividade Concluida.

--- Desafios/Modeloex015/script.js
+++ Desafios/Modeloex015/script.js
@@ -9,11 +9,41 @@ function verificar() {
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
     var genero = ''
-    if(fsex[0].checked) {
-        genero = 'Homem'
-    } else if (fsex[1].checked){
-        genero = 'Mulher'
+    var img = document.createElement('img') //criando inclusão de imagem pelo js//
+    img.setAttribute('id', 'foto') // criando um id para foto é a mesma coisa que voce fosse no html e incluice um id lá//
+    if (fsex[0].checked) {
+      genero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        //criança//
+        img.setAttribute('src', 'fotomeninobebe.png')
+      } else if (idade < 21) {
+        //jovem
+        img.setAttribute('src', 'fotomeninojovem.png')
+      } else if (idade < 50) {
+        //Adulto
+        img.setAttribute('src', 'fotohomemadulto.png')
+      } else {
+        //idoso
+        img.setAttribute('src', 'fotohomemidoso.png')
+      }
+    } else if (fsex[1].checked) {
+      genero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        //criança//
+        img.setAttribute('src', 'fotomeninabebe.png')
+      } else if (idade < 21) {
+        //jovem
+        img.setAttribute('src', 'fotomulherjovem.png')
+      } else if (idade < 50) {
+        //Adulto
+        img.setAttribute('src', 'fotomulheradulta.png')
+      } else {
+        //idoso
+        img.setAttribute('src', 'fotomulheridosa.png')
+      }
     }
+    res.style.textAlign = 'center' //essa linha centraliza o texto
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
   }
 }
