b3ece30e8d8126d7851001e6156dfdaa65fe0988
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-21 23:04:48 -0300

    commit

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,16 +1,15 @@
-let num = [5, 2, 8, 9, 3]
+let num = [5, 8, 2, 9, 3]
 num.push(1) // acrescenta valor no array
 num.sort() // sort coloca o vetor em ordem
 console.log(`o Vetor tem  ${num.length} posições`) // leangth informa quantas posiçoes tem o array
 console.log(num[0])
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 console.log(num)
-let pos = num.indexOf(5)
- if( pos == -1 ) {
-     console.log('o valor não foi encontrado!')
- } else {
-     console.log(`o valor esta na posição ${pos}`)
- }
-
+let pos = num.indexOf(8)
+if (pos == -1) {
+  console.log('o valor não foi encontrado!')
+} else {
+  console.log(`o valor esta na posição ${pos}`)
+}
 
 /*console.log(`O valor 8 esta na posição ${pos}`)*/
