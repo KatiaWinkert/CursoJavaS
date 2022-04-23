5dd342221762fbfbef6a5ce3dd2be8a75f7158d6
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-22 23:32:36 -0300

    commit

--- Desafios/Modeloex18/script.js
+++ Desafios/Modeloex18/script.js
@@ -1,4 +1,28 @@
-function clicar() { 
-    let num = document.querySelector('id#txt1')
-    let Verif = document.querySelector('id#seltab')
+let num = document.querySelector('input#fnum')
+let list = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isnumero(n) {
+if(Number(n) >= 1 && Number(n) <= 100){
+    return true
+}else {
+    return false
+}
+
+}
+ function inlista(n, L) {
+     if(L.indexOf(Number(n)) != -1){
+         return true
+     } else {
+         return false
+     }
+ }
+
+function adicionar(){
+  if (isnumero(num.value) && !inlista(num.value, valores)) {
+      
+  } else {
+    window.alert('Valor inválido ou já esta adicionado na lista.')
+  }
 }
